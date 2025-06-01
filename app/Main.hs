f122eabfe83f644f2d0dbba258a3a912e8fd694f
Toma Sasaki <netst915@gmail.com>
2025-06-01 17:46:15 +0900

    rawnumbertochests

--- app/Main.hs
+++ app/Main.hs
@@ -12,10 +12,15 @@ data Model = Model
     { _rawNumberToShulkerBoxesShulkerBoxes :: Int
     , _rawNumberToShulkerBoxesStacks       :: Int
     , _rawNumberToShulkerBoxesRemains      :: Int
+    , _rawNumberToChestsLargeChests        :: Int
+    , _rawNumberToChestsChests             :: Int
+    , _rawNumberToChestsStacks             :: Int
+    , _rawNumberToChestsRemains            :: Int
     } deriving (Show, Eq)
 
-newtype Action = RawNumberToShulkerBoxesInputUpdate MisoString
-               deriving (Show, Eq)
+data Action = RawNumberToShulkerBoxesInputUpdate MisoString
+            | RawNumberToChestsInputUpdate MisoString
+            deriving (Show, Eq)
 
 makeLenses ''Model
 
@@ -24,6 +29,10 @@ initModel = Model
     { _rawNumberToShulkerBoxesShulkerBoxes = 0
     , _rawNumberToShulkerBoxesStacks       = 0
     , _rawNumberToShulkerBoxesRemains      = 0
+    , _rawNumberToChestsLargeChests        = 0
+    , _rawNumberToChestsChests             = 0
+    , _rawNumberToChestsStacks             = 0
+    , _rawNumberToChestsRemains            = 0
     }
 
 updateModel :: Action -> Effect Model Action
@@ -42,6 +51,23 @@ updateModel (RawNumberToShulkerBoxesInputUpdate rawExpr) =
                 set rawNumberToShulkerBoxesStacks stacks .
                     set rawNumberToShulkerBoxesRemains remains
 
+updateModel (RawNumberToChestsInputUpdate rawExpr) =
+    case evaluateExpr rawExpr of
+        Just value ->
+            let (largeChests, remain1) = value `divMod` (2 * 27 * 64)
+                (chests, remain2) = remain1 `divMod` (27 * 64)
+                (stacks, remain3) = remain2 `divMod` 64 in
+                    submitResult largeChests chests stacks remain3
+        Nothing ->
+            submitResult 0 0 0 0
+    where
+        submitResult :: Int -> Int -> Int -> Int -> Effect Model Action
+        submitResult largeChests chests stacks remains = get >>= put .
+            set rawNumberToChestsLargeChests largeChests .
+                set rawNumberToChestsChests chests .
+                    set rawNumberToChestsStacks stacks .
+                        set rawNumberToChestsRemains remains
+
 evaluateExpr :: MisoString -> Maybe Int
 evaluateExpr str =
     case evaluate mempty (eitherToMaybe $ parse (fromMisoString str)) :: Maybe Double of
@@ -58,11 +84,24 @@ viewModel mdl = div_ []
     , span_ []
         [ text "items = "
         , text (ms $ mdl ^. rawNumberToShulkerBoxesShulkerBoxes)
-        , text "SB + "
+        , text " Shulker Boxes + "
         , text (ms $ mdl ^. rawNumberToShulkerBoxesStacks)
-        , text "st + "
+        , text " Stacks + "
         , text (ms $ mdl ^. rawNumberToShulkerBoxesRemains)
-        , span_ [] []
+        ]
+
+    , h2_ [] [text "Raw Number --> Chests"]
+    , input_ [type_ "text", placeholder_ "Number or expression", onInput RawNumberToChestsInputUpdate]
+    , div_ [class_ "spacer"] []
+    , span_ []
+        [ text "items = "
+        , text (ms $ mdl ^. rawNumberToChestsLargeChests)
+        , text " Large Chests + "
+        , text (ms $ mdl ^. rawNumberToChestsChests)
+        , text " Chests + "
+        , text (ms $ mdl ^. rawNumberToChestsStacks)
+        , text " Stacks + "
+        , text (ms $ mdl ^. rawNumberToChestsRemains)
         ]
 
     , footer_ []
